f8e1d9bd6a236f9203b079e6aff25d551bab9d89
alpineQ <alpineQ@protonmail.com>
2023-12-26 12:19:26 +0300

    ENH: orthographic map

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <WorldMap />
+  <div style="height: 100vh; width: 100wv; background-color: #191b1c;">
+    <WorldMap code="US" :width="500" :height="500" />
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/components/WorldMap.vue
+++ src/components/WorldMap.vue
@@ -4,34 +4,39 @@
 
 <script setup lang="ts">
 import { onMounted, onUnmounted } from 'vue'
-
 import * as am5 from '@amcharts/amcharts5'
 import * as am5map from "@amcharts/amcharts5/map"
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow"
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated"
 
-// import am5geodata_data_countries2 from "@amcharts/amcharts5-geodata/data/countries2";
-// let franceMaps = am5geodata_data_countries2["FR"];
-// console.log(franceMaps)
+
+const props = defineProps<{
+  code: string,
+  width: number,
+  height: number
+}>()
 
 let chart: am5map.MapChart;
+let polygonSeries: am5map.MapPolygonSeries
+let previousPolygon: am5map.MapPolygon | undefined;
 
-function renderChart() {
-  let root = am5.Root.new("chartdiv");
-  root.setThemes([
-    am5themes_Animated.new(root)
-  ]);
+onMounted(() => {
+  const root = am5.Root.new("chartdiv");
+  root.setThemes([am5themes_Animated.new(root)]);
   chart = root.container.children.push(
     am5map.MapChart.new(root, {
-      projection: am5map.geoMercator(),
+      panX: "rotateX",
+      panY: "rotateY",
+      projection: am5map.geoOrthographic(),
     })
   );
-  let polygonSeries = chart.series.push(
+  polygonSeries = chart.series.push(
     am5map.MapPolygonSeries.new(root, {
       geoJSON: am5geodata_worldLow,
-      exclude: ["AQ"]
+      // exclude: ["AQ"]
     })
   );
+
   polygonSeries.mapPolygons.template.setAll({
     tooltipText: "{name}",
     toggleKey: "active",
@@ -46,47 +51,62 @@ function renderChart() {
     fill: root.interfaceColors.get("primaryButtonHover")
   });
 
-  let dataItem = polygonSeries.getDataItemById("RU");
-  console.log(dataItem)
-  // let polygon = countryDataItem.get("mapPolygon");
+  let graticuleSeries = chart.series.unshift(
+    am5map.GraticuleSeries.new(root, {
+      step: 10,
+      stroke: am5.color("#ffffff"),
+    })
+  );
+  graticuleSeries.mapLines.template.set("strokeOpacity", 0.1)
 
-  let previousPolygon: am5map.MapPolygon | undefined;
   polygonSeries.mapPolygons.template.on("active", function (_, target) {
-    if (previousPolygon && previousPolygon != target) {
+    if (previousPolygon && previousPolygon != target)
       previousPolygon.set("active", false);
-    }
-    console.log(target)
     if (target && target.get("active")) {
-      const item = target.dataItem
-      if (item) {
-        console.log(item)
-        polygonSeries.zoomToDataItem(item as am5.DataItem<am5map.IMapPolygonSeriesDataItem>);
+      let centroid = target.geoCentroid();
+      if (centroid) {
+        chart.animate({ key: "rotationX", to: -centroid.longitude, duration: 1500, easing: am5.ease.inOut(am5.ease.cubic) });
+        chart.animate({ key: "rotationY", to: -centroid.latitude, duration: 1500, easing: am5.ease.inOut(am5.ease.cubic) });
       }
+      chart.animate({ key: 'zoomLevel', to: 2, duration: 1500, easing: am5.ease.inOut(am5.ease.cubic) })
+      target.showTooltip()
     }
-    else {
+    else
       chart.goHome();
-    }
     previousPolygon = target;
   });
 
-  chart.set("zoomControl", am5map.ZoomControl.new(root, {}));
-
   chart.appear(1000, 100);
+  setTimeout(() => zoomToCountry(props.code), 1)
+})
+
+onUnmounted(() => chart?.dispose())
+
+function zoomToCountry(code: string) {
+  const country = polygonSeries.getDataItemById(code);
+  if (!country) return
+
+  const polygon = country.getRaw('mapPolygon')
+  previousPolygon = polygon
+  polygon.set("active", true);
+  polygon.showTooltip()
+
+  const centroid = polygon.geoCentroid()
+  if (!centroid) return
+
+  const zoomLevel = code == 'RU' || code == 'US' ? 2 : 3
+  chart.animate({ key: "rotationX", to: -centroid.longitude, duration: 1500, easing: am5.ease.inOut(am5.ease.cubic) });
+  chart.animate({ key: "rotationY", to: -centroid.latitude, duration: 1500, easing: am5.ease.inOut(am5.ease.cubic) });
+  chart.animate({ key: 'zoomLevel', to: zoomLevel, duration: 1500, easing: am5.ease.inOut(am5.ease.cubic) })
 }
 
-onMounted(() => {
-  renderChart();
-})
-onUnmounted(() => {
-  if (chart) {
-    chart.dispose();
-  }
-})
-
 </script>
+
 <style>
 .chartdiv {
-  width: 100%;
+  /* width: v-bind(width)px;
+  height: v-bind(height)px; */
+  width: 500px;
   height: 500px;
 }
 </style>
